fd79d8edb60e505fe70d273da514a1f060a217ed
foy3110 <74569280+foy3110@users.noreply.github.com>
2026-04-24 13:41:39 +0100

    RECONFIG

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from analysis.Analysis import dailySummary
 from analysis.RollingBaseLine import rollingBaseLine, calculate_stress_score, run_analysis
 from analysis.sleepDetector import *
-x = dailySummary()
-print(x)
 run_analysis()
