354d7ff5d0e0df89817f7380aa2babf0a131ba35
foy3110 <74569280+foy3110@users.noreply.github.com>
2026-04-24 13:49:04 +0100

    config again

--- serverFol/Database.py
+++ serverFol/Database.py
@@ -1,22 +1,14 @@
 from mysql.connector import pooling
 from serverFol.Config import DBCONFIG
-import os
-import mysql.connector.pooling
 
 #connector for the serverFol
 def getPool():
-    dbconfig = {
-        "host": os.getenv("DB_HOST"),
-        "port": int(os.getenv("DB_PORT", 3306)),
-        "user": os.getenv("DB_USER"),
-        "password": os.getenv("DB_PASSWORD"),
-        "database": os.getenv("DB_NAME"),
-    }
-
-    pool = mysql.connector.pooling.MySQLConnectionPool(
-        pool_name="mypool",
-        pool_size=5,
-        **dbconfig
+    pool = pooling.MySQLConnectionPool(
+            pool_name=DBCONFIG['pool_name'],
+            pool_size=DBCONFIG['pool_size'],
+            host=DBCONFIG['host'],
+            user=DBCONFIG['user'],
+            password=DBCONFIG['password'],
+            database=DBCONFIG['serverFol'],
     )
-
     return pool
