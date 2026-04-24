516139c2f8b1bb6dd6602223c1718ed965569c5d
foy3110 <74569280+foy3110@users.noreply.github.com>
2026-04-24 13:50:52 +0100

    removed from main

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
 from analysis.Analysis import dailySummary
 from analysis.RollingBaseLine import rollingBaseLine, calculate_stress_score, run_analysis
 from analysis.sleepDetector import *
-run_analysis()
