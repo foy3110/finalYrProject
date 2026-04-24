3553f40a21578dad36f9f6a622f066e5748c81a0
foy3110 <74569280+foy3110@users.noreply.github.com>
2026-04-24 14:00:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from fastapi import FastAPI
+
 from analysis.Analysis import dailySummary
 from analysis.RollingBaseLine import rollingBaseLine, calculate_stress_score, run_analysis
 from analysis.sleepDetector import *
+app = FastAPI()
