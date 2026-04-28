3e2f5730e1daebe5f3ff18f9961b44de3506d481
foy3110 <74569280+foy3110@users.noreply.github.com>
2026-04-28 14:43:07 +0100

    change to config

--- serverFol/Config.py
+++ serverFol/Config.py
@@ -1,11 +1,11 @@
 import os
 
 DBCONFIG = {
-    "host": os.getenv("DB_HOST"),
-    "port": int(os.getenv("DB_PORT" or  3306)),
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
-    "database": os.getenv("DB_NAME"),
+    "host":      os.getenv("DB_HOST"),
+    "port":      int(os.getenv("DB_PORT") or 3306),
+    "user":      os.getenv("DB_USER"),
+    "password":  os.getenv("DB_PASSWORD"),
+    "database":  os.getenv("DB_NAME"),
     "pool_name": "health_pool",
     "pool_size": 5,
 }
